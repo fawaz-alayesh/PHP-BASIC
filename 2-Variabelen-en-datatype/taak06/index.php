53543e06bd0e6a5181d19ad8606d6683938c5a8e
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-06 22:27:49 +0100

    save changes

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php
+$c = "Hello";
+echo gettype($c) . "<br>";
+
+$a = 3;
+echo gettype($a) . "<br>";
+
+$b = 3.2;
+echo gettype($b) . "<br>";
+
+$g = false;
+echo gettype($g) . "<br>";
+
+
+$f = NULL;
+echo gettype($f) . "<br>";
+?>
+
+</body>
+</html>
