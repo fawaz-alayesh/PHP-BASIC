c35ac93bf3b2f614197f171f4ceb5878697aadb5
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-07 20:31:44 +0100

    save changes

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$voornaam1 = "Fawaz";
+$voornaam2 = "Mike";
+$voornaam3 = "Max";
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+?>
+</body>
+</html>
