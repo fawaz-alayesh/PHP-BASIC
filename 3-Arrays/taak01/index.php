b182849c8affa0f665deba4ffb4ce39bf64664d2
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-08 21:56:28 +0100

    save changes

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$myMovies = array("The note", "Home alone", "Tom & Jerry");
+echo "<li>$myMovies[0]</li><li>$myMovies[1]</li><li>$myMovies[2]</li>";
+?>
+
+</body>
+</html>
