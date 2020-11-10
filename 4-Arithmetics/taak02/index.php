92b795a2ff6052e1cb7be34c6fda90bfeb1dcbaf
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-10 17:12:05 +0100

    save changes

---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$getal1 =20;
+echo $getal1 +=10;
+echo "<br>";
+$getal1 =20;
+echo $getal1 -=10;
+echo "<br>";
+$getal1 =20;
+echo $getal1 *=10;
+echo "<br>";
+$getal1 =20;
+echo $getal1 /=10;
+echo "<br>";
+?>
+</body>
+</html>
