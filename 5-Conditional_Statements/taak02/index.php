89c00cb12592e606d28576bdaf3753ad58caa2c2
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-12 14:56:24 +0100

    save changes

--- 5-Conditional_Statements/taak02/index.php
+++ 5-Conditional_Statements/taak02/index.php
@@ -8,15 +8,21 @@ if($test1==$test2) {
 if($test1!=$test2){
     echo"ongelijk" . "<br>";
 }
+
+$test1 = 10;
+$test2 = 10;
 if($test1 ===$test2){
     echo"identiek" . "<br>";
 }
+$test1 = 10;
+$test2 = 6;
 if($test1>$test2){
     echo"test1 is groter dan test2" . "<br>";
 }
 if($test1<$test2){
     echo"test1 is kleiner dan test2" . "<br>";
 }
+
 if($test1>=$test2){
     echo"test1 is groter of gelijk aan test2" . "<br>";
 }
