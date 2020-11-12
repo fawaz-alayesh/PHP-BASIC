b2dec32fdafe841cc4c137aca9ab91382e9e7190
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-12 12:21:38 +0100

    save changes

---
+++ 5-Conditional_Statements/taak05/index.php
@@ -0,0 +1,96 @@
+<?php
+$score = 10;
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!"."<br>";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+$score = 9;
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!"."<br>";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+$score = 8;
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!"."<br>";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+$score = 7;
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!"."<br>";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+$score = 5;
+
+switch($score) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!"."<br>";
+}
+?>
