7f9a7456006c0fbe28b3d70b6beecedcb0f6d8f8
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-12 12:10:57 +0100

    save changes

---
+++ 5-Conditional_Statements/taak04/index.php
@@ -0,0 +1,31 @@
+<?php
+$prod_besteld = 9;
+
+if($prod_besteld < 10) {
+  echo  $prijs = "1.50 " ;
+} elseif($prod_besteld < 20) {
+  echo  $prijs = 1.25;
+} else {
+  echo  $prijs = 1.00;
+}
+
+$prod_besteld = 12;
+
+if($prod_besteld < 10) {
+  echo  $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+  echo  $prijs = 1.25 ;
+} else {
+  echo  $prijs = 1.00;
+}
+
+$prod_besteld = 21;
+
+if($prod_besteld < 10) {
+  echo  $prijs = 1.50;
+} elseif($prod_besteld < 20) {
+  echo  $prijs = 1.25;
+} else {
+  echo  $prijs = " 1.00";
+}
+?>
