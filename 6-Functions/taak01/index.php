cab47c79bf75fc3cbb7a9f4305a0a335dfd14e5e
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2020-11-16 19:38:41 +0100

    save changes

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,14 @@
+<?php
+function zetTweeWoordenAanElkaar(){
+     $woordje='Hallo ';
+     $zinnetje=' mooie blauwe planeet!' ;
+    $heleZin=($woordje.$zinnetje);
+    echo $heleZin;
+
+}
+    zetTweeWoordenAanElkaar();
+
+
+?>
+
+
